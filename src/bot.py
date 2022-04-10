ca8d8c821cda7500affd58f8aec565099e3b7cb1
YouTong0826 <jimmy20070826@gmail.com>
2022-04-10 13:42:44 +0800

    try 16

--- src/bot.py
+++ src/bot.py
@@ -3,16 +3,8 @@ import datetime
 import os
 from discord.ext import commands
 from lib.translate import translate
+from tkinter import *   
 
-try:
-    # for Python2
-    # sudo apt-get install python-tk 
-    from Tkinter import *   ## notice capitalized T in Tkinter 
-except ImportError:
-    # for Python3
-    # sudo apt-get install python3-tk 
-    from tkinter import *   ## notice lowercase 't' in tkinter here
-    
 bot = commands.Bot(
     command_prefix='g!',
     intents=discord.Intents.all()

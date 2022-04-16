36dfc990f77d2669f226221a328acf32908ed50a
YouTong0826 <jimmy20070826@gmail.com>
2022-04-16 21:25:04 +0800

    guild return 2

--- src/commands/cucmd.py
+++ src/commands/cucmd.py
@@ -272,7 +272,7 @@ class Cucmd(Cog_ExtenSion):
 
     @commands.command()
     async def getguild(self,ctx):
-        guilds = [f"{guild}\n" for guild in self.bot.guilds]
+        guilds = [f"{guild.name}/n" for guild in self.bot.guilds]
         
         embed = discord.Embed(
             title="所在的伺服器",

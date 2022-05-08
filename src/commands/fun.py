6a937ddc6cfe82a396af67ec3b23ca05dd898ba2
YouTong0826 <jimmy20070826@gmail.com>
2022-05-09 00:53:20 +0800

    xx

--- src/commands/fun.py
+++ src/commands/fun.py
@@ -201,7 +201,7 @@ class Fun(Cog_ExtenSion):
         embed = discord.Embed(
             title=f"{ctx.author.name} 感謝您使用此功能!",
             description="以下為您的測驗結果",
-            color=0x9b59b6,
+            color=discord.Colour.random()
             timestamp=datetime.datetime.utcnow()
         )
 

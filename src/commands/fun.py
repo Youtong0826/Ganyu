7b9240dfba9182dd925e4186f75e4f38b1a8595d
YouTong0826 <jimmy20070826@gmail.com>
2022-05-09 01:05:16 +0800

    s

--- src/commands/fun.py
+++ src/commands/fun.py
@@ -213,7 +213,7 @@ class Fun(Cog_ExtenSion):
         for n in luckform:
             embed.add_field(name=n,value=luckform[n])
 
-        embed.set_footer("lucktest | 運氣測試",bot_icon_url)
+        embed.set_footer(text="lucktest | 運氣測試",icon_url=bot_icon_url)
 
         await ctx.send(embed=embed)
 

0c3506902f3ac01c5886aa244e7c6f00a99d4e72
Youtong0826 <jimmy2007826@gmail.com>
2024-07-01 10:25:40 +0800

    fix bug

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -1,4 +1,4 @@
-from database import Database
+from database._databse import Database
 # Original repository at https://github.com/Youtong0826/quick-sqlite-db
 # 原始存放庫位於 https://github.com/Youtong0826/quick-sqlite-db
  

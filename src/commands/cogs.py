479d571ea1762bd2e9542ffa6d865154990a3b87
Youtong0826 <jimmy2007826@gmail.com>
2024-07-01 09:56:26 +0800

    add `cogs.py` at commands

---
+++ src/commands/cogs.py
@@ -0,0 +1,58 @@
+from lib.cog import CogExtension
+from discord import (
+    ApplicationContext as Context,
+    Embed,
+    slash_command
+)
+
+class SlashCogs(CogExtension):
+    @slash_command()
+    async def load(self, ctx: Context, folder: str, extension=None):
+        if ctx.author.id not in [611118369474740244, 856041155341975582]: 
+            return await ctx.response.send_message('❌ 此為開發者功能!', ephemeral=True)
+
+        if not extension:
+            self.bot.load_extension(folder)
+
+        self.bot.load_extension(f"{folder}.{extension}")
+        
+        await ctx.send(embed=Embed(
+            title=f"Loaded - {folder}.{extension} - Cog",
+            color=0x5cff8d
+        ))
+        
+        self.bot.log(ctx)
+        
+    @slash_command()
+    async def unload(self, ctx: Context, folder: str, extension=None):
+        if ctx.author.id not in [611118369474740244, 856041155341975582]: 
+            return await ctx.response.send_message('❌ 此為開發者功能!', ephemeral=True)
+
+        if not extension:
+           self.bot.load_extension(folder, "unload")
+
+        self.bot.unload_extension(f"{folder}.{extension}")
+        
+        await ctx.send(embed=Embed(
+            title=f"Unloaded - {folder}.{extension} - Cog",
+            color=0x5cff8d
+        ))
+        
+        self.bot.log()
+
+    @slash_command()
+    async def reload(self, ctx: Context, folder: str, extension=None):
+        if ctx.author.id not in [611118369474740244, 856041155341975582]: 
+            return await ctx.response.send_message('❌ 此為開發者功能!', ephemeral=True)
+
+        if not extension:
+            self.bot.load_extension(folder, "reload")
+
+        self.bot.reload_extension(f"{folder}.{extension}")
+        
+        await ctx.send(embed=Embed(
+            title=f"Reloaded - {folder}.{extension} - Cog",
+            color=0x5cff8d
+        ))
+        
+        self.bot.log()

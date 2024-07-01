fdbc530e2a77914eca49c0cf3dbcaf920ea8fa5b
Youtong0826 <jimmy2007826@gmail.com>
2024-07-01 09:19:11 +0800

    add rpc, fix choose role

--- src/events/interaction.py
+++ src/events/interaction.py
@@ -1,36 +1,43 @@
-from lib.classes import CogExtension
-from discord.ext import commands
-import discord
+from discord import (
+    Cog,
+    Colour,
+    Embed,
+    EmbedField,
+    Interaction
+)
+import random
+
+from lib.cog import CogExtension
+from lib.role import choose_role
 
 class InteractionEvent(CogExtension):
+    @Cog.listener()
+    async def on_interaction(self,interaction:Interaction):
+        if interaction.is_command(): return
 
-    @commands.Cog.listener()
-    async def on_interaction(self,interaction:discord.Interaction):
-        if interaction.is_command():return
-
-        if interaction.custom_id.endswith("ping"):
-            roles = interaction.user.guild.roles
+        custom_id = interaction.custom_id
+        if custom_id.endswith("ping"):
             if interaction.custom_id == "PA_ping":
-                for role in roles :
-                    if role.id == 962261741050413096:
-                        if role in interaction.user.roles:
-                            await interaction.user.remove_roles(role)
-                            await interaction.response.send_message(content=f"已成功移除身分組✅",ephemeral=True)
-
-                        else:
-                            await interaction.user.add_roles(role)
-                            await interaction.response.send_message(content=f"已成功領取身分組✅",ephemeral=True)
+                await choose_role(interaction.user, 962261741050413096)
 
             if interaction.custom_id == "Bu_ping":  
-                for role in roles :
-                    if role.id == 1009478887140511915:
-                        if role in interaction.user.roles:
-                            await interaction.user.remove_roles(role)
-                            await interaction.response.send_message(content=f"已成功移除身分組✅",ephemeral=True)
+                await choose_role(interaction.user, 1009478887140511915)
+                
+            return await interaction.response.send_message(content=f"已成功變更身分組✅",ephemeral=True)
+    
+        if custom_id == "rpc_punch":
+            result = random.choice([
+                ["你輸了..", "下次再來吧!"],
+                ["平手!", "勢均力敵呢!"],
+                ["你贏了!!", "幹得不錯嘛!"]    
+            ])
 
-                        else:
-                            await interaction.user.add_roles(role)
-                            await interaction.response.send_message(content=f"已成功領取身分組✅",ephemeral=True)
+            await interaction.response.edit_message(embed=Embed(
+                title = result[0],
+                description = result[1],
+                color = Colour.random()
+            ))
+        
 
 def setup(bot):
     bot.add_cog(InteractionEvent(bot))

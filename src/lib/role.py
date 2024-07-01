53f7702a7f79d628fc9b9fd40c21533bdcc89b40
Youtong0826 <jimmy2007826@gmail.com>
2024-07-01 09:18:38 +0800

    add `choose_role()`

---
+++ src/lib/role.py
@@ -0,0 +1,7 @@
+from discord import User, Member
+
+async def choose_role(user: User | Member, id: int):
+    role = filter(lambda x: x.id == id, user.guild.roles)[0]
+    await (user.remove_roles(role) if role in user.roles else user.add_roles(role))
+    
+    

361c679f89b1c82ffdf4f8e7cb9a8fba6da09aee
Youtong0826 <jimmy2007826@gmail.com>
2024-07-02 20:51:43 +0800

    fix some tab

--- src/lib/role.py
+++ src/lib/role.py
@@ -1,7 +1,5 @@
 from discord import User, Member
 
 async def choose_role(user: User | Member, id: int):
-    role = filter(lambda x: x.id == id, user.guild.roles)[0]
+    role = list(filter(lambda x: x.id == id, user.guild.roles))[0]
     await (user.remove_roles(role) if role in user.roles else user.add_roles(role))
-    
-    
